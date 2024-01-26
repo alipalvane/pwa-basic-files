63faea7e1daa90e50c32586b4da0373045ccc26a
Ali <alipalvane@gmail.com>
2024-01-26 15:05:14 +0330

    add dynamic verion in cache

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
+const cacheVersion = 3;
+const activeCaches = {
+  "pwa-cache": `pwa-${cacheVersion}V`,
+};
 // ------- LIFECYCLE  -----------
 //1. Install SW on Client's Browser
 self.addEventListener("install", (event) => {
@@ -6,7 +10,7 @@ self.addEventListener("install", (event) => {
 
   event.waitUntil(
     // install assets in cache of client's browser in first load page
-    caches.open("pwa-cache").then((cache) => {
+    caches.open(activeCaches["pwa-cache"]).then((cache) => {
       //Added signle files in cache with "add" method
       // cache.add('./js/app.js')
       // cache.add('./style.css')
@@ -23,6 +27,18 @@ self.addEventListener("install", (event) => {
 //2. Activated SW on Client's Browser
 self.addEventListener("activate", (event) => {
   console.log("server worker activated");
+
+  //Remove Extra and old caches from client's browser when we add new cache in app
+  const activeCacheNames = Object.values(activeCaches);
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(cacheNames.forEach((cacheName) => {
+        if(!activeCacheNames.includes(cacheName)){
+          return caches.delete(cacheName);
+        }
+      }));
+    })
+  );
 });
 // ------- LIFECYCLE  -----------
 
